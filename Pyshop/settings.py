5a71683b34edc0612b49975c7a7b58dcd4989889
Navneet <46026280+navghosh@users.noreply.github.com>
2021-06-24 06:56:09 +0530

    Updated

--- Pyshop/settings.py
+++ Pyshop/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'bgmt5)bm(y$9^#5ns$=40@+-w#w2j0zlld0(9#*f5&qkq@d3ca'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
